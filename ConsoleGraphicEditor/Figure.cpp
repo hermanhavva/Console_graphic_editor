4ade6f62770f34b5a30db916fb9262deed82a116
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-10-07 23:45:34 +0300

    figure.cpp refactored, the problem is still present
    E0079 problem

--- ConsoleGraphicEditor/Figure.cpp
+++ ConsoleGraphicEditor/Figure.cpp
@@ -11,22 +11,17 @@ using namespace std;
 
 
 
-class Figure abstract
-{
-public:
-	Figure(const COORD& startPos, const WORD& colour)
-	{
-		id++;
-		this->startPos = startPos;
-		this->colour = colour;
-		idToFigurePtrMap[id] = this;
-		figTypeEnum = DEFAULT;
-	}
-	virtual ~Figure() = default;
 
-	virtual string GetFigProperties() = 0;
+Figure::Figure(const COORD& startPos, const WORD& colour)
+{
+	id++;
+	this->startPos = startPos;
+	this->colour = colour;
+	idToFigurePtrMap[id] = this;
+	figTypeEnum = DEFAULT;
+}
 	
-	int SelectFigById(const size_t& id)
+int Figure::SelectFigById(const size_t& id)
 	{
 		if (!idToFigurePtrMap.contains(id))
 		{
@@ -50,33 +45,22 @@ public:
 		return -1;
 	}
 	
-	FIGURE_TYPE GetType() const
+FIGURE_TYPE Figure::GetType() const
 	{
 		return this->figTypeEnum;
 	}
 
-	static deque <Figure*> GetAllFigsCoordsInDrawOrder() 
+deque <Figure*> Figure::GetAllFigsCoordsInDrawOrder() 
 	{
 		return figDrawOrderDeque;
 	}
-	inline static string& GetConfigurationStr(); 
 	
-	unordered_set<COORD, COORDHash, COORDEqual> GetThisFigCoordsSet() const 
-	{
-		return this->figureCoordSet;
-	}
-protected:
+unordered_set<COORD, COORDHash, COORDEqual> Figure::GetThisFigCoordsSet() const 
+{
+	return this->figureCoordSet;
+}
 
-	FIGURE_TYPE figTypeEnum;
-	COORD startPos;
-	char symbolToDraw = '*';
-	static inline size_t id = 0;
-	inline static deque <Figure*> figDrawOrderDeque;
-	WORD colour; 
-	unordered_set<COORD, COORDHash, COORDEqual> figureCoordSet;
-	inline static unordered_map<size_t, Figure*> idToFigurePtrMap;
-
-	bool AreSetsEqual(const unordered_set <COORD, COORDHash, COORDEqual>& inSet1, const unordered_set<COORD, COORDHash, COORDEqual>& inSet2)
+bool Figure::AreSetsEqual(const unordered_set <COORD, COORDHash, COORDEqual>& inSet1, const unordered_set<COORD, COORDHash, COORDEqual>& inSet2)
 	{
 		if (inSet1.size() != inSet2.size())
 		{
@@ -92,67 +76,53 @@ protected:
 		return true;
 	}
 
-};
-
-class Rectangle : public Figure
+Rectangle::Rectangle(const size_t& width, const size_t& height, const COORD& startPos, const WORD& colour)
+:	width(width),
+	height(height),
+	Figure(startPos, colour) 
 {
-public:
-	Rectangle(const size_t& width, const size_t& height, const COORD& startPos, const WORD& colour)
-		:	width(width),
-			height(height),
-			Figure(startPos, colour) 
+	for (COORD curCoord = startPos; curCoord.X <= startPos.X + width; curCoord.X++)  // horizontal
 	{
-		for (COORD curCoord = startPos; curCoord.X <= startPos.X + width; curCoord.X++)  // horizontal
-		{
-			figureCoordSet.insert(curCoord);
-			curCoord.Y += height;
-			figureCoordSet.insert(curCoord);
-			curCoord.Y -= height;
-		}
-		for (COORD curCoord = startPos; curCoord.Y <= startPos.Y + height; curCoord.Y++)  // vertical
-		{
-			figureCoordSet.insert(curCoord);
-			curCoord.X += width;
-			figureCoordSet.insert(curCoord);
-			curCoord.X -= width;
-		}
-
-		figDrawOrderDeque.push_back(this);  // push new instance to the back
-		figTypeEnum = RECTANGLE;
+		figureCoordSet.insert(curCoord);
+		curCoord.Y += height;
+		figureCoordSet.insert(curCoord);
+		curCoord.Y -= height;
 	}
+	for (COORD curCoord = startPos; curCoord.Y <= startPos.Y + height; curCoord.Y++)  // vertical
+	{
+		figureCoordSet.insert(curCoord);
+		curCoord.X += width;
+		figureCoordSet.insert(curCoord);
+		curCoord.X -= width;
+	}
+	figDrawOrderDeque.push_back(this);  // push new instance to the back
+	figTypeEnum = RECTANGLE;
+}
 	
 	//virtual ~Rectangle() = default;
-	string GetFigProperties() override
-	{
-		return format(" {} {} {} {} {} ", startPos.X, startPos.Y, width, height, colour);
-	}
-
-protected:
-	const size_t width;
-	const size_t height;
-};
-
-class Square : public Rectangle
+string Rectangle::GetFigProperties() 
 {
-public:	
-	Square(const size_t& side, 
-		   const COORD& startPos, 
-		   const WORD& colour)
-		: Rectangle(side*2, side, startPos, colour) 
-	{
-		figTypeEnum = SQUARE;
-	}
-	string GetFigProperties() override
-	{
-		return format(" {} {} {} {} ", startPos.X, startPos.Y, height, colour);
-	}
-};
-class Triangle : public Figure
+	return format(" {} {} {} {} {} ", startPos.X, startPos.Y, width, height, colour);
+}
+
+
+Square::Square(const size_t& side, 
+			   const COORD& startPos, 
+			   const WORD& colour)
+:	Rectangle(side*2, side, startPos, colour) 
 {
-public:
-	Triangle(const size_t& base, 
-			 const COORD& startPos, 
-			 const WORD& colour)
+	figTypeEnum = SQUARE;
+}
+
+string Square::GetFigProperties()
+{
+	return format(" {} {} {} {} ", startPos.X, startPos.Y, height, colour);
+}
+
+
+Triangle::Triangle(const size_t& base, 
+	 			   const COORD& startPos, 
+				   const WORD& colour)
 		: base(base),
 		Figure(startPos, colour)  // here calling base class constructor
 	{
@@ -189,61 +159,54 @@ public:
 		figTypeEnum = TRIANGLE;
 	}
 
-	string GetFigProperties() override
-	{
-		return format(" {} {} {} {} ", startPos.X, startPos.Y, base, colour);
-	}
-private:
-	size_t base;
-	
-};
-class Circle : public Figure
+string Triangle::GetFigProperties() 
 {
-public:
-	Circle(const COORD& startPos, const size_t& radius, const WORD& colour)
-		:	radius(radius), 
-			Figure(startPos, colour)
+	return format(" {} {} {} {} ", startPos.X, startPos.Y, base, colour);
+}
+	
+
+
+Circle::Circle(const COORD & startPos, const size_t & radius, const WORD & colour)
+	:	radius(radius), 
+		Figure(startPos, colour)
+{
+	
+	COORD curCOORD{startPos.X - radius + 1, startPos.Y - radius - 1};
+	for (;curCOORD.X <= startPos.X + radius - 1; curCOORD.X ++)
 	{
-		
-		COORD curCOORD{startPos.X - radius + 1, startPos.Y - radius - 1};
-
-		for (;curCOORD.X <= startPos.X + radius - 1; curCOORD.X ++)
-		{
-			this->figureCoordSet.insert(curCOORD);
-			COORD tempCOORD{ curCOORD.X, curCOORD.Y + 2 * radius + 2 };
-			this->figureCoordSet.insert(tempCOORD);
-		}
-		
-		curCOORD.X = startPos.X - radius - 1;
-		curCOORD.Y = startPos.Y - radius + 2;
-
-		for (; curCOORD.Y <= startPos.Y + radius - 2; curCOORD.Y ++)
-		{
-			this->figureCoordSet.insert(curCOORD);
-			COORD tempCOORD{ curCOORD.X + radius + 2, curCOORD.Y};
-			this->figureCoordSet.insert(tempCOORD);
-		}
-		
-		for (int i = 1; i <= 2; i ++)
-		{
-			COORD temp{ startPos.X - radius, startPos.Y + radius * pow(-1, i) };
-			this->figureCoordSet.insert(temp);
-			temp.X += 2 * radius;
-			this->figureCoordSet.insert(temp);
-		}
-
-		figDrawOrderDeque.push_back(this);
+		this->figureCoordSet.insert(curCOORD);
+		COORD tempCOORD{ curCOORD.X, curCOORD.Y + 2 * radius + 2 };
+		this->figureCoordSet.insert(tempCOORD);
 	}
-	string GetFigProperties() override
+	
+	curCOORD.X = startPos.X - radius - 1;
+	curCOORD.Y = startPos.Y - radius + 2;
+
+	for (; curCOORD.Y <= startPos.Y + radius - 2; curCOORD.Y ++)
 	{
-		return format(" {} {} {} {} ", startPos.X, startPos.Y, radius, colour);
+		this->figureCoordSet.insert(curCOORD);
+		COORD tempCOORD{ curCOORD.X + radius + 2, curCOORD.Y};
+		this->figureCoordSet.insert(tempCOORD);
 	}
-private:
-	size_t radius;
-};
+		
+	for (int i = 1; i <= 2; i ++)
+	{
+		COORD temp{ startPos.X - radius, startPos.Y + radius * pow(-1, i) };
+		this->figureCoordSet.insert(temp);
+		temp.X += 2 * radius;
+		this->figureCoordSet.insert(temp);
+	}
+
+	figDrawOrderDeque.push_back(this);
+}
+string Circle::GetFigProperties() 
+{
+	return format(" {} {} {} {} ", startPos.X, startPos.Y, radius, colour);
+}
 
 
- string& Figure::GetConfigurationStr()
+
+string& Figure::GetConfigurationStr()
 {
 	string result;
 	string type;
